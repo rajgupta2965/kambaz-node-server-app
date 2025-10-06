a893e69dc8857931e89ec480c6d2b166dc2de45c
Raj Gupta <rajgupta2965@gmail.com>
2025-10-05 22:36:41 -0400

    users: enrolled course fix

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -89,41 +89,73 @@ export default function UserRoutes(app) {
     res.json(newCourse);
   };
 
+  // const findCoursesForUser = async (req, res) => {
+  //   const currentUser = req.session["currentUser"];
+  //   if (!currentUser) {
+  //     res.sendStatus(401);
+  //     return;
+  //   }
+  //   if (currentUser.role === "ADMIN") {
+  //     const courses = await courseDao.findAllCourses();
+  //     res.json(courses);
+  //     return;
+  //   }
+  //   let { uid } = req.params;
+  //   if (uid === "current") {
+  //     uid = currentUser._id;
+  //   }
+  //   const courses = await enrollmentsDao.findCoursesForUser(uid);
+  //   res.json(courses);
+  // };
+
+  // const enrollUserInCourse = async (req, res) => {
+  //   let { uid, cid } = req.params;
+  //   if (uid === "current") {
+  //     const currentUser = req.session["currentUser"];
+  //     uid = currentUser._id;
+  //   }
+  //   const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
+  //   res.send(status);
+  // };
+
+  // const unenrollUserFromCourse = async (req, res) => {
+  //   let { uid, cid } = req.params;
+  //   if (uid === "current") {
+  //     const currentUser = req.session["currentUser"];
+  //     uid = currentUser._id;
+  //   }
+  //   const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
+  //   res.send(status);
+  // };
+
+  ////////////FIX//////////////
   const findCoursesForUser = async (req, res) => {
     const currentUser = req.session["currentUser"];
-    if (!currentUser) {
-      res.sendStatus(401);
-      return;
-    }
+    if (!currentUser) return res.sendStatus(401);
+    let { uid } = req.params;
+    if (!uid || uid === "current") uid = currentUser._id;
     if (currentUser.role === "ADMIN") {
       const courses = await courseDao.findAllCourses();
-      res.json(courses);
-      return;
-    }
-    let { uid } = req.params;
-    if (uid === "current") {
-      uid = currentUser._id;
+      return res.json(courses);
     }
     const courses = await enrollmentsDao.findCoursesForUser(uid);
     res.json(courses);
   };
 
   const enrollUserInCourse = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) return res.sendStatus(401);
     let { uid, cid } = req.params;
-    if (uid === "current") {
-      const currentUser = req.session["currentUser"];
-      uid = currentUser._id;
-    }
+    if (!uid || uid === "current") uid = currentUser._id;
     const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
     res.send(status);
   };
 
   const unenrollUserFromCourse = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) return res.sendStatus(401);
     let { uid, cid } = req.params;
-    if (uid === "current") {
-      const currentUser = req.session["currentUser"];
-      uid = currentUser._id;
-    }
+    if (!uid || uid === "current") uid = currentUser._id;
     const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
     res.send(status);
   };
@@ -133,14 +165,17 @@ export default function UserRoutes(app) {
   app.post("/api/users", createUser);
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
-
   app.post("/api/users/signin", signin);
   app.post("/api/users/signup", signup);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
   app.post("/api/users/current/courses", createCourse);
-
   app.get("/api/users/:uid/courses", findCoursesForUser);
   app.post("/api/users/:uid/courses/:cid", enrollUserInCourse);
   app.delete("/api/users/:uid/courses/:cid", unenrollUserFromCourse);
+
+  ////////////////FIX///////////////////
+  app.get("/api/users/current/courses", findCoursesForUser);
+  app.post("/api/users/current/courses/:cid", enrollUserInCourse);
+  app.delete("/api/users/current/courses/:cid", unenrollUserFromCourse);
 }
